829c6fd7a04c89d23efff2a740f00e4e1bd697b2
treiter317 <mvy3ns@virginia.edu>
2023-05-31 17:23:54 +0100

    added my fibonacci function to main.js

--- main.js
+++ main.js
@@ -20,6 +20,23 @@ function printFibonnacci() {
     }
 }
 
+//Thomas
+function fibonacci (num) {
+    let prev = 0;
+    let current = 1;
+    let counter = 1;
+    if(num != 0){
+        console.log(current);
+        while(counter < num) {
+            let next = prev + current;
+            console.log(next);
+            prev = current;
+            current = next;
+            counter++;
+        }
+    }
+}
 
 console.log(fibbonaci10());
 console.log(printFibonnacci());
+fibonacci(10);
