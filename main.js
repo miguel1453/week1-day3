ff48e717927afc0e8ac26562a6728bbaf9dd5c76
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2023-05-31 11:52:23 +0100

    day3 hw

---
+++ main.js
@@ -0,0 +1,14 @@
+const todaysDate = () => {
+    console.log(new Date());
+    };
+    todaysDate();
+    
+    
+    const isSqaured = (x) => {
+        let root = Math.sqrt(x);
+        return root % 1 == 0;
+    };
+    console.log(isSqaured(10));
+    console.log(isSqaured(9));
+    
+    
